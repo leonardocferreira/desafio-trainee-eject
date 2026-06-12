790c491408b3829fb580bc97c3ee69fbcbafd1da
leonardo <privado.leocarlos@gmail.com>
2026-06-12 02:22:03 -0300

    feat: adiciona models Order e OrderItem (não concluido)

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from user.models import UserModel
 
-# Create your models here.
+class Order(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'PENDING', 'Pending'
+        CONFIRMED = 'CONFIRMED', 'Confirmed'
+        SHIPPED = 'SHIPPED', 'Shipped'
+        DELIVERED = 'DELIVERED', 'Delivered'
+        CANCELLED = 'CANCELLED', 'Cancelled'
+
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='orders')
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING, verbose_name='Status')
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        pass
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Order Item'
+        verbose_name_plural = 'Order Items'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        pass
